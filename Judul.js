6a488ae00b1019d359537b6e1ee0978f09f6677a
soemantry <42644398+soemantry@users.noreply.github.com>
2018-09-14 11:39:43 +0700

    props, biodata-login-kelas revisi

--- Judul.js
+++ Judul.js
@@ -23,29 +23,4 @@ const salon = {
 
 	}
 }
-=======
-import React, { Component } from 'react';
-import {Text,View} from 'react-native';
 
-export default class Judul extends Component {
-	render(){
-		return  (
-			<View>
-			
-			<Text style={salon.Judul}> BIODATA </Text>
-
-			</View>
-			)
-	}
-}
-const salon = {
-	Judul: {
-		Color: '#f00',
-		fontSize: 50,
-		fontWeight: 'bold',
-		backgroundColor: '#0f0'
-		
-
-	}
-}
->>>>>>> 4b5010b38808d4f90b14b68b060227bf1c19f7de
